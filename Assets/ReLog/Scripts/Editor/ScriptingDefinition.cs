551487646312760dd459ae0d1e96862b32253fb4
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2025-04-19 17:08:41 -0400

    add RELOG scripting definition

---
+++ Assets/ReLog/Scripts/Editor/ScriptingDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace ReLog.Editor
+{
+    [InitializeOnLoad]
+    internal class ScriptingDefinition
+    {
+        private const string RELOG_SD = "RELOG";
+
+        static ScriptingDefinition() => AddScriptingDefineSymbol(RELOG_SD);
+        
+        private static void AddScriptingDefineSymbol(string define)
+        {
+            BuildTargetGroup buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            string[] defines;
+            PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup, out defines);
+            List<string> clone = new List<string>(defines);
+            if(!clone.Contains(define))
+                clone.Add(define);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, clone.ToArray());
+        }
+    }
+}
